879515188686ea1eb18373c5913069ebf31dd35a
JustAgent <baykov.ivan.03@gmail.com>
2022-09-07 20:56:27 +0300

    4 screens

--- Лаба 4/task_3/main.py
+++ Лаба 4/task_3/main.py
@@ -1,51 +1,43 @@
-import random
+def rabin_karp(pattern, txt, prime):
+	M = len(pattern)
+	N = len(txt)
+	i = 0
+	j = 0
+	p = 0 
+	t = 0 
+	h = 1
 
-def RabinKarp(text, pattern):
-	p = 1000000009
-	x = random.randint(1, p - 1)
+	for i in range(M-1):
+		h = (h * d)% prime
 
-	result = []
-	# pHash = PolyHash(P, p, x)
+	
+	for i in range(M):
+		p = (d * p + ord(pattern[i]))% prime
+		t = (d * t + ord(txt[i]))% prime
 
-	H = PrecomputeHashes(text, len(pattern), p, x)
+	for i in range(N-M + 1):
+		if p == t:
+			for j in range(M):
+				if txt[i + j] != pattern[j]:
+					break
 
-	for i in range(0, len(text) - len(pattern)):
-		if pHash != H[i]:
-			continue
+			j+= 1
+			if j == M:
+				print(str(i+1))
 
-		if text[i:i+len(pattern)] == pattern:
-			result.append(i)
+		if i < N-M:
+			t = (d*(t-ord(txt[i])*h) + ord(txt[i + M]))% prime
 
-	return result
+			
+			if t < 0:
+				t = t + prime
 
-def PrecomputeHashes(text, pattern_length, p, x):
-	H = [0 for _ in range(len(text) - pattern_length + 1)]
-
-	S = text[len(text) - pattern_length:len(text)]
-	# H[len(text) - pattern_length] = PolyHash(S,p,x)
-	y = 1
-
-	for i in range(1, pattern_length):
-		y = (y * x) % p
-
-	for i in range(len(text) - pattern_length - 1, -1, -1):
-		H[i] = (x * H[i + 1] + text[i] - y * text[i + pattern_length]) % p
-
-	return H
 
 with open("input.txt", "r") as input_file:
 	pattern = input_file.readline().strip()
 	text = input_file.readline()
 
-result = RabinKarp(text, pattern)
-print(result)
+d = 256
+prime = 1000000009 
+rabin_karp(pattern, text, prime)
 
-with open("output.txt", "w") as output_file:
-	output_file.write(str(len(result)) + "\n")
-
-	output_indeces = ""
-	for index in result:
-		output_indeces += str(index + 1) + " "
-	output_file.write(output_indeces)
-
-	
